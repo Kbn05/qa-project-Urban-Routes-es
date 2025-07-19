727f152d59f9ccb2828de02c2c737fcd1975959d
Kevin <kpaboncarvajal@gmail.com>
2025-07-19 13:24:24 -0500

    Fix #1: Added asserts & fixed POM

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-20feecbd-c081-43e6-bb20-e5e67a8d6a9b.containerhub.tripleten-services.com/?lng=es'
+urban_routes_url = 'https://cnt-ceb67886-3a30-4213-bfce-8cac93d16561.containerhub.tripleten-services.com/?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
